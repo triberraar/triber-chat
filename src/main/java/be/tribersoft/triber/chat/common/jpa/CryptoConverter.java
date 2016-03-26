70236d8399a6f7811d7a4f135e67d482c0be5c88
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-03-26 18:56:59 +0100

    improved the encryption/decryption

--- src/main/java/be/tribersoft/triber/chat/common/jpa/CryptoConverter.java
+++ src/main/java/be/tribersoft/triber/chat/common/jpa/CryptoConverter.java
@@ -1,41 +1,20 @@
 package be.tribersoft.triber.chat.common.jpa;
 
-import java.security.Key;
-import java.util.Base64;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 @Converter
 public class CryptoConverter implements AttributeConverter<String, String> {
-	private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
-
-	private static final byte[] KEY = "MySuperSecretKey".getBytes();
 
 	@Override
 	public String convertToDatabaseColumn(String rawData) {
-		Key key = new SecretKeySpec(KEY, "AES");
-		try {
-			Cipher cipher = Cipher.getInstance(ALGORITHM);
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-			return Base64.getEncoder().encodeToString(cipher.doFinal(rawData.getBytes()));
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		return AESCipher.encrypt(rawData);
+
 	}
 
 	@Override
 	public String convertToEntityAttribute(String dbData) {
-		Key key = new SecretKeySpec(KEY, "AES");
-		try {
-			Cipher cipher = Cipher.getInstance(ALGORITHM);
-			cipher.init(Cipher.DECRYPT_MODE, key);
-			return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		return AESCipher.decrypt(dbData);
 	}
 
 }
--- src/main/java/be/tribersoft/triber/chat/common/jpa/CryptoSecret.java
+++ src/main/java/be/tribersoft/triber/chat/common/jpa/CryptoSecret.java
@@ -8,13 +8,28 @@ import org.springframework.beans.factory.annotation.Value;
 @Named
 public class CryptoSecret {
 
-	public static String SECRET;
+	private static String SECRET;
+	private static String IV;
 
 	@Value("${crypto.secret}")
-	private String cryptoSecret;
+	private String secret;
+	@Value("${crypto.iv}")
+	private String iv;
 
 	@PostConstruct
 	public void init() {
-		SECRET = cryptoSecret;
+		SECRET = secret;
+		IV = iv;
+		if (SECRET.length() != 16 || IV.length() != 16) {
+			throw new RuntimeException("Wrong setup in crypto, both secret and iv should be 16 chars long");
+		}
+	}
+
+	public static String getSecret() {
+		return SECRET;
+	}
+
+	public static String getIv() {
+		return IV;
 	}
 }
