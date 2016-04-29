fba5698e9a3f52c87da0b5e8c2e63b3f16725005
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-04-29 20:02:20 +0200

    karma test

---
+++ src/main/resources/static/js/helloworld_spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('test', function() {
+    it('shouldnt work', function() {
+        expect(true).toEqual(false);
+    });
+    it('should work', function() {
+        expect(true).toEqual(true);
+    });
+});
