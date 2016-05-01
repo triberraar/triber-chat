3cff1767cd02dead59d7ae53efabee8540a5c3bb
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-05-01 20:51:42 +0200

    test

--- src/main/resources/static/js/common/lodash_spec.js
+++ src/main/resources/static/js/common/lodash_spec.js
@@ -2,21 +2,14 @@
 
 describe('_', function() {
 
-    var windowMock = {
-        _ : 'lodash'
-    };
-
+    var _;
     beforeEach(module('_'));
-
-    beforeEach(module(function($provide) {
-        $provide.value('$window', windowMock);
-    }));
 
     beforeEach(inject(function(___) {
         _ = ___;
     }));
 
     it('should return lodash', function() {
-        expect(_).toEqual('lodash');
+        expect(_).toBeDefined();
     });
 });
