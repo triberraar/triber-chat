0994abf92e720283c4d0816f693c0f0bea93137b
triberraar <geertolaerts@gmail.com>
2016-06-21 20:04:16 +0200

    Feature/08 upgrades (#8)
    * updated versions in pom
    
    * updated frontend dependencies
    
    * linting errors
    bithound ignore?

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ var express = require('express');
 var http      = require( 'http' );
 var path = require('path');
 var bunyan = require('bunyan');
-var logger = bunyan.createLogger({name: "proxy"});
+var logger = bunyan.createLogger({name: 'proxy'});
 
 var proxy = httpProxy.createProxyServer({
     target: 'http://127.0.0.1:8080' // where do we want to proxy to?
@@ -27,11 +27,11 @@ proxy.on( 'error', function ( err ) {
    logger.error({name: 'proxy error', err:err});
 });
 
-proxy.on( 'proxyReq', function ( proxyReq, req, res ) {
+proxy.on( 'proxyReq', function ( proxyReq) {
     logger.info({name: 'proxyReq', path: proxyReq.path});
 });
 
-proxy.on( 'proxyReqWs', function ( proxyReqWs, req, res ) {
+proxy.on( 'proxyReqWs', function ( proxyReqWs ) {
     logger.info({name: 'proxyReq WS', path: proxyReqWs.path});
 });
 
