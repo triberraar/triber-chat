a49f97b05d423d0719671cd334b87c85cbc31cdd
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-08-01 17:47:30 +0200

    test fix?

--- src/test/java/be/tribersoft/triber/chat/message/controller/MessageControllerPrivateChatIT.java
+++ src/test/java/be/tribersoft/triber/chat/message/controller/MessageControllerPrivateChatIT.java
@@ -106,7 +106,7 @@ public class MessageControllerPrivateChatIT extends AbstractWebsocketIT {
 		connect(fromHandler, FROM);
 		connect(toHandler, TO);
 
-		if (!latch.await(25, TimeUnit.SECONDS)) {
+		if (!latch.await(50, TimeUnit.SECONDS)) {
 			fail("timeout");
 		}
 		if (failure.get() != null) {
