a750e81ca12f5aaa2fb7b06ba5d9895842add353
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-08-29 21:22:11 +0200

    lets release on develop attempt 9

--- travis-docker.sh
+++ travis-docker.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -ev
 echo $TRAVIS_TAG
+echo $TRAVIS_BRANCH
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
 	echo "dev"
 else
 	echo "no dev"
 fi
+
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" == "develop" ]; then
+	ECHO "go" 
+else 
+	echo "no go" 
+fi
+
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" == "develop" ]; then 
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
     docker build -t triberraar/triber-chat:latest -t triberraar/triber-chat:$TRAVIS_TAG .
