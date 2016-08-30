66319e73116fc08a74bd7ab1b2f71c2fed442611
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-08-30 20:05:26 +0200

    travis github release attempt 4

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM openjdk:8-jdk-alpine
 
-COPY ./target/triber-chat-0.0.1-SNAPSHOT.jar app.jar
+ARG tag
+
+COPY ./target/triber-chat-$tag.jar app.jar
 RUN sh -c 'touch /app.jar'
 
 ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/app.jar"]
--- travis-docker.sh
+++ travis-docker.sh
@@ -2,6 +2,6 @@
 set -ev
 if [ ! -z "$TRAVIS_TAG" ]; then 
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-    docker build -t triberraar/triber-chat:latest -t triberraar/triber-chat:$TRAVIS_TAG .
+    docker build --build-arg tag=$TRAVIS_TAG -t triberraar/triber-chat:latest -t triberraar/triber-chat:$TRAVIS_TAG .
     docker push triberraar/triber-chat
 fi
